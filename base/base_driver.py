12fae9012cb8c523a5c14e657e354fd096e8e94a
jiandong.li <jiandong.li@qq.com>
2020-03-29 21:38:38 +0800

    all 1

--- base/base_driver.py
+++ base/base_driver.py
@@ -1,12 +1,17 @@
 from appium import webdriver
 
-def get_driver(noReset=True):
-    desired_caps = {}
-    desired_caps['platformName'] = 'Android' 		#指定类型
-    desired_caps['platformVersion'] = '6.0.1'		#模拟器版本
-    desired_caps['deviceName'] = '127.0.0.1:7555'	#模拟器名      android的可以随意写（安全起见，按照标准来），ios必须写标准
-    desired_caps['appPackage'] = 'com.yunmall.lc'#apk名
-    desired_caps['appActivity'] = 'com.yunmall.ymctoc.ui.activity.MainActivity'		#首启动
-    desired_caps['noReset'] = noReset                 #True 不重置应用
-    return webdriver.Remote('http://localhost:4723/wd/hub',desired_caps) #默认的值
 
+def init_driver(no_reset=True):
+    desired_caps = dict()
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.1'
+    desired_caps['deviceName'] = '192.168.56.101:5555'
+    desired_caps['appPackage'] = 'com.yunmall.lc'
+    desired_caps['appActivity'] = 'com.yunmall.ymctoc.ui.activity.MainActivity'
+    # toast
+    desired_caps['automationName'] = 'Uiautomator2'
+    # 是否重置应用 True:不重置 False:重置
+    desired_caps['noReset'] = no_reset
+
+    driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+    return driver
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,20 +1,38 @@
-#ModuleNotFoundError: No module named 'base' 当使用pytest框架运行时出现导包错误，使用下面两句话
-import os,sys
-sys.path.append(os.getcwd())
+import time
 import pytest
-from base.base_driver import get_driver
-from time import sleep
 
-class TestLogin():
+from base.base_analyze import analyze_file
+from base.base_driver import init_driver
+from page.page import Page
+
+
+class TestLogin:
 
     def setup(self):
-        self.driver=get_driver(True)
+        self.driver = init_driver(no_reset=False)
+        self.page = Page(self.driver)
 
     def teardown(self):
-        sleep(5)
+        time.sleep(2)
         self.driver.quit()
-        pass
 
+    @pytest.mark.parametrize("args", analyze_file("login_data.yaml", "test_login"))
+    def test_login(self, args):
 
-    def test_login(self):
-        print("aaa")
+        # 解析 yaml 的数据
+        username = args["username"]
+        password = args["password"]
+        toast = args["toast"]
+
+        # 脚本流程
+        self.page.home.click_me()
+        self.page.register.click_login()
+        self.page.login.input_username(username)
+        self.page.login.input_password(password)
+        self.page.login.click_login()
+
+        if toast is None:
+            assert self.page.me.get_nick_name_text() == username, "登录后的用户名和输入的用户民不一致"
+        else:
+            # 找toast提示，找args中的toast提示是否能找到，如果能则通过，如果不能则不通过
+            assert self.page.login.is_toast_exist(toast)
